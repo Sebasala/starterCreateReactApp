67120a59d9139414f06d3aea2f71d266f8c07796
Sebastian Salazar <sebastian.salazar@ariadnacg.com>
2019-02-22 15:50:12 -0500

    First commit to the repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Starter Create React App
+
+This is a create react app customized with Sass 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello React!</h1>
+    );
+  }
+}
+
+export default App;
